8fa60a5bc4906f80c14f4c62a8371948e882c987
SahilAglawe <sahilaglawe66@gmail.com>
2026-05-30 03:02:25 +0530

    write business logic

--- src/main/java/com/example/myRESTAPIs/controller/studentController.java
+++ src/main/java/com/example/myRESTAPIs/controller/studentController.java
@@ -1,13 +1,23 @@
 package example.myRESTAPIs.controller;
 
 import example.myRESTAPIs.dto.StudentDto;
+import example.myRESTAPIs.service.StudentService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
 
+@RestController
+@RequiredArgsConstructor
 public class studentController{
 
-    @GetMapping("/student")
-    public StudentDto getStudent(){
-        return new StudentDto(4L, "sahil" , "sahil@gmail.com");
+    private final StudentService studentService;
+
+
+
+    @GetMapping("/students")
+    public List<StudentDto> getAllStudent(){
+        return studentService.getAllStudents();
     }
 
     @GetMapping("/student/{id}")
