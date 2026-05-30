99220d348cf031c96b5a5ba3f4b8a76cda421b8d
SahilAglawe <sahilaglawe66@gmail.com>
2026-05-30 09:38:53 +0530

    write apis

--- src/main/java/com/example/myRESTAPIs/controller/studentController.java
+++ src/main/java/com/example/myRESTAPIs/controller/studentController.java
@@ -1,10 +1,13 @@
 package example.myRESTAPIs.controller;
 
+import example.myRESTAPIs.dto.AddStudentRequestDto;
 import example.myRESTAPIs.dto.StudentDto;
 import example.myRESTAPIs.service.StudentService;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
 @RestController
@@ -13,15 +16,24 @@ public class studentController{
 
     private final StudentService studentService;
 
-
-
     @GetMapping("/students")
     public List<StudentDto> getAllStudent(){
         return studentService.getAllStudents();
     }
 
-    @GetMapping("/student/{id}")
-    public StudentDto getStudentById(){
-        return new StudentDto(4L, "sahil" , "sahil@gmail.com");
+    @GetMapping("/students/{id}")
+    public StudentDto getStudentById(@PathVariable Long id){
+         return  studentService.getStudentById(id);
+    }
+
+    @PostMapping("/students")
+    public ResponseEntity<StudentDto> createStudent(@RequestBody AddStudentRequestDto addStudentRequestDto){
+        return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createNewStudent(addStudentRequestDto));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteStudent(@PathVariable Long id){
+        studentService.deleteStudentById(id);
+        return ResponseEntity.noContent().build();
     }
 }
