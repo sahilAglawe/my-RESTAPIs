b08c459d059fe6548da5913904e7708075f38968
SahilAglawe <sahilaglawe66@gmail.com>
2026-05-30 11:28:10 +0530

    Done

--- src/main/java/com/example/myRESTAPIs/controller/studentController.java
+++ src/main/java/com/example/myRESTAPIs/controller/studentController.java
@@ -3,31 +3,34 @@ package example.myRESTAPIs.controller;
 import example.myRESTAPIs.dto.AddStudentRequestDto;
 import example.myRESTAPIs.dto.StudentDto;
 import example.myRESTAPIs.service.StudentService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/students")
 public class studentController{
 
     private final StudentService studentService;
 
-    @GetMapping("/students")
+    @GetMapping
     public List<StudentDto> getAllStudent(){
         return studentService.getAllStudents();
     }
 
-    @GetMapping("/students/{id}")
+    @GetMapping("/{id}")
     public StudentDto getStudentById(@PathVariable Long id){
          return  studentService.getStudentById(id);
     }
 
-    @PostMapping("/students")
-    public ResponseEntity<StudentDto> createStudent(@RequestBody AddStudentRequestDto addStudentRequestDto){
+    @PostMapping
+    public ResponseEntity<StudentDto> createStudent(@RequestBody @Valid  AddStudentRequestDto addStudentRequestDto){
         return ResponseEntity.status(HttpStatus.CREATED).body(studentService.createNewStudent(addStudentRequestDto));
     }
 
@@ -36,4 +39,14 @@ public class studentController{
         studentService.deleteStudentById(id);
         return ResponseEntity.noContent().build();
     }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<StudentDto> updateStudent(@PathVariable Long id, @RequestBody AddStudentRequestDto addStudentRequestDto){
+        return ResponseEntity.ok(studentService.updateStudent(id, addStudentRequestDto));
+    }
+
+    @PatchMapping("/{id}")
+    public ResponseEntity<StudentDto> updatePartialStudent(@PathVariable Long id, @RequestBody Map<String,Object> updates){
+        return ResponseEntity.ok(studentService.updatePartialStudent(id, updates));
+
 }
